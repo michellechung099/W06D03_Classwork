aa31557100888a79f40eff50ec9d9872ee58bcfc
mchung099 <mchung099@gmail.com>
2023-03-08 15:08:33 -0800

    basic setup

---
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  # resources :users
+
+  get '/users', to: 'users#index'
+  post '/users', to: 'users#create'
+  get '/users/new', to: 'users#new'
+  get '/users/:id/edit', to: 'users#edit', as:'new_user_edit'
+  get '/users/:id', to: 'users#show'
+  patch '/users/:id', to: 'users#update'
+  put '/users/:id', to: 'users#update'
+  delete '/users/:id', to: 'users#destroy'
+end
