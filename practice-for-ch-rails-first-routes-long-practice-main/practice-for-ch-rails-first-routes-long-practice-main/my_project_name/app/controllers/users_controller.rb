a7ec4621fa0a5b4ca8afed57a7ded12959bb57e3
brandon <brandon.w.leung@gmail.com>
2023-03-08 15:27:43 -0800

    create and show

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
+require "byebug"
 class UsersController < ApplicationController
   def index
+    # debugger
     render plain: "I'm in the index action"
+  end
+
+  def create 
+    render json: params
+  end
+
+  def show 
+    render json: params
   end
 end
