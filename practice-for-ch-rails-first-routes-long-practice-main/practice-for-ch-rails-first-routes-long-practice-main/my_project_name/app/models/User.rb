d7a3d114e664927a6b9c561c150b7edeeced87b0
mchung099 <mchung099@gmail.com>
2023-03-08 22:43:17 -0800

    annotate added

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/models/User.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/models/User.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  email      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 #  create_table "users", force: :cascade do |t|
 #     t.string "username", null: false
 #     t.datetime "created_at", null: false
---
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/models/artworkshare.rb
@@ -0,0 +1,3 @@
+class Artworkshare < ApplicationRecord
+
+end
