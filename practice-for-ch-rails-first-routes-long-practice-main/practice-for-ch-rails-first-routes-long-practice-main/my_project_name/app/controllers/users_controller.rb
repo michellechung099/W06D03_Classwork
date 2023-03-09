dd0fd594303fff55e9741c3baa17d80a5aeae4f6
mchung099 <mchung099@gmail.com>
2023-03-08 17:01:22 -0800

    controller practice done

--- practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/app/controllers/users_controller.rb
@@ -2,14 +2,52 @@ require "byebug"
 class UsersController < ApplicationController
   def index
     # debugger
-    render plain: "I'm in the index action"
+    users = User.all
+    render json: users
   end
 
-  def create 
-    render json: params
+  def create
+    user = User.new(user_param)
+    if user.save
+      redirect_to users_url(user.id)
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+    # render json: user
   end
 
-  def show 
-    render json: params
+  def show
+    incoming_wildcard = params[:id]
+    user = User.find_by(id: incoming_wildcard)
+    if user
+      render json: user
+    else
+      render json: user.errors.full_messages, status: 404
+    end
+  end
+
+  def update
+    incoming_wildcard = params[:id]
+    user = User.find_by(id: incoming_wildcard)
+    if user && user.update(user_param)
+      redirect_to users_url
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    incoming_wildcard = params[:id]
+    user = User.find_by(id: incoming_wildcard)
+    if user && user.destroy
+      redirect_to users_url
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def user_param
+    params.require(:user).permit(:name, :email)
   end
 end
---
+++ practice-for-ch-rails-first-routes-long-practice-main/practice-for-ch-rails-first-routes-long-practice-main/my_project_name/db/migrate/20230309001708_create_fix_unique.rb
@@ -0,0 +1,7 @@
+class CreateFixUnique < ActiveRecord::Migration[7.0]
+  def change
+    add_index :users, :name, unique: true
+
+    add_index :users, :email, unique: true
+  end
+end
